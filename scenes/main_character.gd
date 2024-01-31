fa75214a6e1e87b769e1420463d53abbcdcc4772
Júlia S. Coité <juliascoite@gmail.com>
2024-01-31 12:55:23 -0300

    Init

---
+++ scenes/main_character.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+
+const SPEED = 400.0
+const JUMP_VELOCITY = -900.0
+@onready var sprite_2d = $Sprite2D
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 15)
+		
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		sprite_2d.animation = "jumping"
+		
+	elif (velocity.x > 1 || velocity.x < -1):
+			sprite_2d.animation = "running"
+	elif (velocity.x == 0):
+			sprite_2d.animation = "default"
+	
+
+	
+	
+	move_and_slide()
+	
